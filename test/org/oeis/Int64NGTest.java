de490198732eef247170491fb14672620bad6fde
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-13 23:35:19 -0500

    New test class

---
+++ test/org/oeis/Int64NGTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.oeis;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class Int64NGTest {
+    
+    public Int64NGTest() {
+    }
+
+    /**
+     * Test of toInt8 method, of class Int64.
+     */
+    @Test
+    public void testToInt8() {
+        System.out.println("toInt8");
+        Int64 instance = null;
+        Int8 expResult = null;
+        Int8 result = instance.toInt8();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toInt16 method, of class Int64.
+     */
+    @Test
+    public void testToInt16() {
+        System.out.println("toInt16");
+        Int64 instance = null;
+        Int16 expResult = null;
+        Int16 result = instance.toInt16();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toInt32 method, of class Int64.
+     */
+    @Test
+    public void testToInt32() {
+        System.out.println("toInt32");
+        Int64 instance = null;
+        Int32 expResult = null;
+        Int32 result = instance.toInt32();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toInt64 method, of class Int64.
+     */
+    @Test
+    public void testToInt64() {
+        System.out.println("toInt64");
+        Int64 instance = null;
+        Int64 expResult = null;
+        Int64 result = instance.toInt64();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
