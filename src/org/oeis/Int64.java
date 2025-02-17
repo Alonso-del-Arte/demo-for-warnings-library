9ef5646ccd3df94a0eed889c5e5b6133f44bc710
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-17 16:53:14 -0500

    Annotated three functions with narrowing conversion warning

--- src/org/oeis/Int64.java
+++ src/org/oeis/Int64.java
@@ -8,23 +8,27 @@ package org.oeis;
 import org.testframe.annotations.warnings.NarrowingConversionWarning;
 
 /**
- *
+ * Wraps a 64-bit integer primitive.
  * @author Alonso del Arte
  */
 public class Int64 extends IntegerWrapper {
     
-//    @NarrowingConversionWarning(sourceType = Int64.class, 
-//            targetType = Int8.class)
+    @NarrowingConversionWarning(sourceType = Int64.class, 
+            targetType = Int8.class)
     @Override
     public Int8 toInt8() {
         return new Int8((byte) this.heldLong);
     }
     
+    @NarrowingConversionWarning(sourceType = Int64.class, 
+            targetType = Int16.class)
     @Override
     public Int16 toInt16() {
         return new Int16((short) this.heldLong);
     }
     
+    @NarrowingConversionWarning(sourceType = Int64.class, 
+            targetType = Int32.class)
     @Override
     public Int32 toInt32() {
         return new Int32((int) this.heldLong);
